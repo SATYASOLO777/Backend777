edef05df2cf148cba08986e0d76a1fee3207f7fb
SATYA SOLO <satyasolo317@gmail.com>
2025-01-20 19:13:56 +0530

    Update backend CORS settings and API routes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, request, send_from_directory
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "https://movies-homee.netlify.app"}})
 # Mock movie data
 movies = [
     {'title': 'Pirates of the Caribbean', 'genre': 'Adventure', 'imageUrl': 'static/images/imagepoc.jpg', 'moviePath': 'static/videos/Pirates.mp4'},
@@ -10,8 +10,13 @@ movies = [
     {'title': 'Movie 4', 'genre': 'Horror', 'imageUrl': 'static/images/movie4.jpg', 'moviePath': 'static/videos/movie4.mp4'},
     {'title': 'Movie 5', 'genre': 'Adventure', 'imageUrl': 'static/images/movie5.jpg', 'moviePath': 'static/videos/movie5.mp4'}
 ]
-
-
+@app.route('/api/movies/<int:movie_id>', methods=['GET'])
+def get_movie_by_id(movie_id):
+    movie = movies[movie_id]  # Assuming movie_id matches the index in the list
+    if movie:
+        return jsonify(movie)
+    else:
+        return "Movie not found", 404
 # API endpoint to get movies
 @app.route('/api/movies', methods=['GET'])
 def get_movies():
@@ -22,14 +27,11 @@ def get_movies():
         if (not genre or movie['genre'] == genre) and
            (not search or search in movie['title'].lower())
     ]
-
     return jsonify(filtered_movies)
-
 @app.route('/watch', methods=['GET'])
 def watch_movie():
     title = request.args.get('title')
     movie = next((movie for movie in movies if movie['title'] == title), None)
-    
     if movie:
         return f'''
         <html>
@@ -44,27 +46,22 @@ def watch_movie():
         '''
     else:
         return "Movie not found", 404
-
 @app.route('/download', methods=['GET'])
 def download_movie():
     title = request.args.get('title')
     movie = next((movie for movie in movies if movie['title'] == title), None)
-    
     if movie:
         filename = movie['moviePath'].split('/')[-1]  # Extract the filename from the path
         return send_from_directory('static/videos', filename, as_attachment=True)
     else:
         return "Movie not found", 404
-
 # Serve static files
 @app.route('/<path:path>', methods=['GET'])
 def static_files(path):
     return send_from_directory('', path)
-
 # Serve the homepage
 @app.route('/')
 def index():
     return send_from_directory('', 'index.html')
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
